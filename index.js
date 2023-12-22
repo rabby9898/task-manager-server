fc785828942622656ac5ce3034dc643ebb9fb2e2
rabby9898 <mdfajlerabby00@gmail.com>
2023-12-22 19:39:23 +0600

    server cors fixed

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 3000;
-const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
-  credentials: true,
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: [
+//     "http://localhost:5173",
+//     "http://localhost:5174",
+//     "https://your-task-manager-f.web.app/",
+//     "https://server-alpha-lyart.vercel.app/",
+//   ],
+
+//   optionSuccessStatus: 200,
+// };
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qczjssr.mongodb.net/?retryWrites=true&w=majority`;
@@ -59,12 +64,12 @@ async function run() {
       res.send(result);
     });
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
